c1b9c34c5c229cea6ad489b7c32e7686c308b482
tschachner28 <53120075+tschachner28@users.noreply.github.com>
2021-01-05 15:41:25 -0500

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+    <h1>Successful initialization of your project! May the force be with you!</h1>
+    </div>
+  );
+}
+
+export default App;
